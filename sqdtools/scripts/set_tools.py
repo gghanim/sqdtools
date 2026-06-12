f6dbe793e80cb3dc3202d300b064976b86dd5922
George Ghanim <gghanim@princeton.edu>
2026-06-11 22:50:33 -0400

    added particles vs micrographs to set_tools

--- sqdtools/scripts/set_tools.py
+++ sqdtools/scripts/set_tools.py
@@ -62,9 +62,9 @@ def unique():
 @click.option('--i', '--input', 'input_file', multiple=True, required=True, type=click.Path(exists=True, resolve_path=False), help="Path to the input .star file", metavar='<starfile.star>')
 @click.option('--u', '--unique', 'operation', flag_value='unique', default=True)
 @click.option('--n', '--intersect', 'operation', flag_value='intersect')
-#@click.option('--data_column', 'data_column', default='rlnDefocusU', show_default=True, type=str, help="RELION data column to select. \"list\" will print valid data column names.", metavar='<rlnDataColumn>')
+@click.option('--data_column', 'data_column', multiple=True, required=True, type=str, help="RELION data column to select. \"list\" will print valid data column names.", metavar='<rlnDataColumn>')
 #@click.option('--o', '--output', 'out', is_flag=False, flag_value="histogram_output.pdf", help="Optional name for the output file.", metavar='<output.pdf>')
-def cli(input_file, operation):
+def cli(input_file, operation, data_column):
 
     file_list = []
     for file in input_file:
@@ -110,27 +110,55 @@ def cli(input_file, operation):
         click.echo(f'\n  Intersecting files...')
         click.echo(f'    \"{input_file[0]}\" is file A.')
         click.echo(f'    \"{input_file[1]}\" is file B.')
-        dfA, dfB = file_list[0]['particles'], file_list[1]['particles']
 
-        click.echo(f'\n    {len(dfA):,} particles in A.')
-        click.echo(f'    {len(dfB):,} particles in B.')
+        if isinstance(file_list[0], pd.DataFrame):
+            dfA, A_type = file_list[0], 'list'
+            click.echo(f'\n    {len(dfA):,} items in A.')
+        elif isinstance(file_list[0], dict):
+            if 'micrographs' in file_list[0].keys():
+                dfA, A_type = file_list[0]['micrographs'], 'micrographs'
+                click.echo(f'\n    {len(dfA):,} micrographs in A.')
+            elif 'particles' in file_list[0].keys():
+                dfA, A_type = file_list[0]['particles'], 'particles'
+                click.echo(f'\n    {len(dfA):,} particles in A.')
 
-        AnB = dfA[dfA['rlnImageName'].isin(dfB['rlnImageName'])]
-        AnB_starfile = {
-        'optics' : file_list[0]['optics'],
-        'particles': AnB
-        }
-        starfile.write(AnB_starfile, "AnB_keepingA.star")
-        click.echo(f'    {len(AnB):,} particles in A intersect B.')
+        if isinstance(file_list[1], pd.DataFrame):
+            dfB, B_type = file_list[1], 'list'
+            click.echo(f'    {len(dfB):,} items in B.')
+        elif isinstance(file_list[1], dict):
+            if 'micrographs' in file_list[1].keys():
+                dfB, B_type = file_list[1]['micrographs'], 'micrographs'
+                click.echo(f'    {len(dfB):,} micrographs in B.')
+            elif 'particles' in file_list[1].keys():
+                dfB, B_type = file_list[1]['particles'], 'particles'
+                click.echo(f'    {len(dfB):,} particles in B.')
 
-        BnA = dfB[dfB['rlnImageName'].isin(dfA['rlnImageName'])]
-        BnA_starfile = {
-        'optics' : file_list[1]['optics'],
-        'particles': BnA
-        }
-        starfile.write(BnA_starfile, "AnB_keepingB.star")
 
-        click.echo(f'\n  Wrote A intersect B (keeping A) to \"AnB_keepingA.star\".')
-        click.echo(f'  Wrote A intersect B (keeping B) to \"AnB_keepingB.star\".')
+        data_columns = list(set(data_column))
+
+        AnB = dfA[dfA['rlnMicrographName'].isin(dfB['rlnMicrographName'])]
+        if A_type == 'list':
+            click.echo(f'\n  File A is not the usual RELION STAR format. Skipping writing...')
+        else:
+            AnB_starfile = {
+            'optics' : file_list[0]['optics'],
+            A_type: AnB
+            }
+            starfile.write(AnB_starfile, "AnB_keepingA.star")
+            click.echo(f'    {len(AnB):,} particles in A intersect B.')
+            click.echo(f'\n  Wrote A intersect B (keeping A) to \"AnB_keepingA.star\".')
+
+        BnA = dfB[dfB['rlnMicrographName'].isin(dfA['rlnMicrographName'])]
+        if B_type == 'list':
+            click.echo(f'\n  File B is not the usual RELION STAR format. Skipping writing...')
+        else:
+            BnA_starfile = {
+            'optics' : file_list[1]['optics'],
+            B_type: BnA
+            }
+            starfile.write(BnA_starfile, "AnB_keepingB.star")
+            click.echo(f'    {len(AnB):,} particles in A intersect B.')
+            click.echo(f'  Wrote A intersect B (keeping B) to \"AnB_keepingB.star\".')
+
 if __name__ == '__main__':
     cli(max_content_width=120)
